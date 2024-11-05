d6ac7d978e4d7f020cec146bc22281d9c58b4431
Dev-Pavithan <pavithanunenthiran29@gmail.com>
2024-11-05 16:08:16 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,34 +1,19 @@
-// // index.js
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
-// import './i18n';
-// import { LanguageProvider } from './LanguageContext.js';
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <BrowserRouter>
-//     <LanguageProvider>
-
-//       <App />
-//     </LanguageProvider>
-
-//   </BrowserRouter>
-// );
-
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import './i18n';
 import { LanguageProvider } from './LanguageContext.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <LanguageProvider>
+
       <App />
     </LanguageProvider>
-  </React.StrictMode>
+
+  </BrowserRouter>
 );
